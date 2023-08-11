91a846ca18c799be84efc76bf1a42205bd2cce2f
eckry <112786854+eckry@users.noreply.github.com>
2023-08-10 18:21:53 -0600

    Various fixes

--- api/server.js
+++ api/server.js
@@ -1,11 +1,19 @@
-import express from "express"
-import mongoose from "mongoose"
+import express from "express";
+import mongoose from "mongoose";
+import Note from "./schemas/Note.js";
 
-app = express();
+const app = express();
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/todos", () => console.log("Connected to MongoDB"));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todos")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+const PORT = process.env.PORT ?? 3001;
 
-const PORT = process.env.PORT ?? 3001
+app.get("/todos", async (req, res) => {
+  const notes = await Note.find();
+  res.json(notes);
+});
 
 app.listen(PORT, () => console.log("Server initialized"));
