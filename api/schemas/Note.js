6d9383d2efc39dab0dbc17f0646c147da9228147
eckry <112786854+eckry@users.noreply.github.com>
2023-08-11 17:29:32 -0600

    feat: note-title-property

--- api/schemas/Note.js
+++ api/schemas/Note.js
@@ -1,6 +1,7 @@
 import { Schema, model } from "mongoose";
 
 const NoteSchema = new Schema({
+  title: { type: String, required: true },
   text: { type: String, required: true },
   completed: { type: Boolean, default: false },
   color: { type: String, default: "white" },
