25a7e5ed551747de7f6739d61079eb3e5cabfe6c
eckry <112786854+eckry@users.noreply.github.com>
2023-08-11 18:11:50 -0600

    feat: get keywords and post them
    Delete funcionality for notes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import Note from "./schemas/Note.js";
+import Keyword from "./schemas/Keyword.js";
 
 const app = express();
 app.use(express.json());
@@ -18,6 +19,11 @@ app.get("/todos", async (req, res) => {
   res.json(notes);
 });
 
+app.get("/todos/keywords", async (req, res) => {
+  const keywords = await Keyword.find();
+  res.json(keywords);
+});
+
 //* POST
 
 app.post("/todos/add", async (req, res) => {
@@ -26,13 +32,19 @@ app.post("/todos/add", async (req, res) => {
   res.json(newNote);
 });
 
-//* PUT 
+app.post("/todos/keywords/add", async (req, res) => {
+  const newKeyword = new Keyword(req.body);
+  await newKeyword.save();
+  res.json(newKeyword);
+});
+
+//* PUT
 
 app.put("/todos/edit/:id", async (req, res) => {
   const noteToUpdate = await Note.findById(req.params.id);
-  const {title, text} = req.body;
+  const { title, text } = req.body;
   noteToUpdate.title = title;
-  noteToUpdate.text = text
+  noteToUpdate.text = text;
   await noteToUpdate.save();
   res.json(noteToUpdate);
 });
@@ -60,3 +72,10 @@ app.put("/todos/new-keywords/:id/:keywords", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log("Server initialized"));
+
+//* DELETE
+
+app.delete("/todos/delete/:id", async (req, res) => {
+  const noteDeleted = await Note.findByIdAndDelete(req.params.id);
+  res.json(noteDeleted);
+});
