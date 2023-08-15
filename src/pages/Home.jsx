e9f59bbf596d70f51e0d0bae0f654ed2b554a19f
eckry <112786854+eckry@users.noreply.github.com>
2023-08-14 21:12:54 -0600

    making filters functionality part 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,15 @@ const API_BASE = "http://localhost:3001/";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
+  const [keyword, setKeyword] = useState("");
+  const [filters, setFilters] = useState({
+    word: "",
+    keywords: [],
+  });
 
   useEffect(() => {
     getNotes();
+    getKeywords();
   }, []);
 
   async function getNotes() {
@@ -17,24 +23,53 @@ export default function Home() {
     setNotes(data);
   }
 
+  async function getKeywords() {
+    const res = await fetch(`${API_BASE}todos/keywords`);
+    const data = await res.json();
+    setFilters((filters) => ({ ...filters, keywords: data }));
+  }
+
+  function filterNotes(notes) {
+    const regExp = new RegExp(filters.word, "i");
+    const filteredNotes = notes.filter((note) => {
+      return regExp.test(note.title) && (note.keyword === keyword || !keyword);
+    });
+    return filteredNotes;
+  }
+
+  const filteredNotes = filterNotes(notes);
+
   return (
     <main>
       <header className="header">
-        <h1 className="header-title">Your <span>Notes</span> App</h1>
+        <h1 className="header-title">
+          Your <span>Notes</span> App
+        </h1>
       </header>
       <div className="utils">
         <div className="filters">
           <p>Word filter: </p>
-          <input type="text" />
+          <input
+            type="text"
+            value={filters.word}
+            onChange={(e) =>
+              setFilters((filters) => ({ ...filters, word: e.target.value }))
+            }
+          />
           <p>Keyword filter:</p>
-          <select>
-            <option value="">Hola</option>
+          <select onChange={(e) => setKeyword(e.target.value)}>
+            <option value=""></option>
+            {filters.keywords.map((keyword) => (
+              <option key={keyword.keyword} value={keyword.keyword}>
+                {keyword.keyword}
+              </option>
+            ))}
           </select>
         </div>
         <button className="add-note">+</button>
       </div>
       <div className="notes-container">
-        {notes.map((note) => {
+        {filteredNotes.map((note) => {
           return (
             <Note
               key={note._id}
